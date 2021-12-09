2ebb54d14cef8ef4a47e5efe1aefdaf8fe4c7bfd
Siberis <siberis@outlook.com>
2021-12-09 06:24:13 +0100

    Day9 - 2 Stars

---
+++ Day9/Day9.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9
+{
+    public static class Day9
+    {
+        public static int Star1(string[] input)
+        {
+            int res = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < input[0].Length; j++)
+                {
+                    if (
+                        (j == 0 || input[i][j] < input[i][j - 1]) &&
+                        (i == 0 || input[i][j] < input[i - 1][j]) &&
+                        (j == input[0].Length - 1 || input[i][j] < input[i][j + 1]) &&
+                        (i == input.Length - 1 || input[i][j] < input[i + 1][j])
+                    )
+                    {
+                        res += input[i][j] - '0' + 1;
+                    }
+                }
+            }
+            return res;
+        }
+        public static int Star2(string[] input)
+        {
+            var basins = new List<int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < input[0].Length; j++)
+                {
+                    if (
+                        (j == 0 || input[i][j] < input[i][j - 1]) &&
+                        (i == 0 || input[i][j] < input[i - 1][j]) &&
+                        (j == input[0].Length - 1 || input[i][j] < input[i][j + 1]) &&
+                        (i == input.Length - 1 || input[i][j] < input[i + 1][j])
+                    )
+                    {
+                        var temp = FindBasin(input.Select(e => e.ToArray()).ToArray(), i, j);
+                        basins.Add(temp);
+                    }
+                }
+            }
+            var res = basins.OrderByDescending(e => e).Take(3).ToArray();
+            return res[0] * res[1] * res[2];
+        }
+
+        private static int FindBasin(char[][] input, int x, int y)
+        {
+            if (input[x][y] == '9')
+            {
+                return 0;
+            }
+            input[x][y] = 'x';
+            int res = 0;
+            if (x > 0 && input[x - 1][y] != 'x')
+            {
+                res += FindBasin(input, x - 1, y);
+            }
+            if (x < input.Length - 1 && input[x + 1][y] != 'x')
+            {
+                res += FindBasin(input, x + 1, y);
+            }
+            if (y > 0 && input[x][y - 1] != 'x')
+            {
+                res += FindBasin(input, x, y - 1);
+            }
+            if (y < input[0].Length - 1 && input[x][y + 1] != 'x')
+            {
+                res += FindBasin(input, x, y + 1);
+            }
+            return res + 1;
+        }
+    }
+}
---
+++ Day9/Day9Test.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Utils;
+using Xunit;
+
+namespace Day9
+{
+    public class Day9Test
+    {
+        [Fact]
+        public async Task Test1()
+        {
+            var lines = await ReadUtils.ReadAllLines("./Day9Part1.txt").ConfigureAwait(false);
+            var parts = lines.Select(e => e).ToArray();
+
+            Assert.Equal(15, Day9.Star1(parts));
+        }
+        [Fact]
+        public async Task Star1()
+        {
+            var lines = await ReadUtils.ReadAllLines("./Day9Part1Input.txt").ConfigureAwait(false);
+            var parts = lines.Select(e => e).ToArray();
+
+            Assert.Equal(491, Day9.Star1(parts));
+        }
+        [Fact]
+        public async Task Test2()
+        {
+            var lines = await ReadUtils.ReadAllLines("./Day9Part1.txt").ConfigureAwait(false);
+            var parts = lines.Select(e => e).ToArray();
+
+            Assert.Equal(1134, Day9.Star2(parts));
+        }
+        [Fact]
+        public async Task Star2()
+        {
+            var lines = await ReadUtils.ReadAllLines("./Day9Part1Input.txt").ConfigureAwait(false);
+            var parts = lines.Select(e => e).ToArray();
+
+            Assert.Equal(0, Day9.Star2(parts));
+        }
+    }
+}
